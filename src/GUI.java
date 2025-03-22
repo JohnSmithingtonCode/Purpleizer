9a2fa526497ed1943bd5c0fbb4dcc78bd5c64a76
LeslieNeko <papageorgiouleslie@gmail.com>
2025-03-23 01:42:05 +1030

    Image scaling
    -Added image scaling functionality, so we can now fit the image properly in the main window

--- src/GUI.java
+++ src/GUI.java
@@ -10,7 +10,7 @@ public class GUI extends JFrame {
     public GUI() {
         // Set up the JFrame
         setTitle("Image Display");
-        setSize(500, 500);
+        setSize(500, 500);  // Initial size of the window
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
@@ -44,13 +44,41 @@ public class GUI extends JFrame {
         add(openButton, BorderLayout.SOUTH);
     }
 
-    // Method to display the image in the JLabel
+    // Method to display the image in the JLabel, scaled to fit within the window
     private void displayImage(File file) {
-        ImageIcon imageIcon = new ImageIcon(file.getAbsolutePath());
-        imageLabel.setIcon(imageIcon);
+        ImageIcon originalIcon = new ImageIcon(file.getAbsolutePath());
+        Image originalImage = originalIcon.getImage();
+
+        // Get the current size of the window
+        int width = getWidth();
+        int height = getHeight();
+
+        // Calculate the new width and height to maintain aspect ratio and fit within the window
+        int newWidth = width;
+        int newHeight = (int) (originalImage.getHeight(null) * ((double) newWidth / originalImage.getWidth(null)));
+
+        // If the new height exceeds the window height, adjust based on the height
+        if (newHeight > height) {
+            newHeight = height;
+            newWidth = (int) (originalImage.getWidth(null) * ((double) newHeight / originalImage.getHeight(null)));
+        }
+
+        // Scale the image
+        Image scaledImage = originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+
+        // Set the scaled image icon to the JLabel
+        imageLabel.setIcon(scaledIcon);
         revalidate();
         repaint();
     }
 
-
+    public static void main(String[] args) {
+        // Create and show the GUI
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new GUI().setVisible(true);
+            }
+        });
+    }
 }
